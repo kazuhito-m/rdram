59c78c83e6334195c907fd21c7355640dd542f7c
kazuhito_m <kazuhito.sumpic@gmail.com>
2020-07-24 22:52:52 +0900

    debug: いろいろな「Draw2dで出来るやり方」を 試してみる場所を充実させていく。

--- pages/layouttest.vue
+++ pages/layouttest.vue
@@ -8,7 +8,7 @@
     <v-icon>$vuetify.icons.company</v-icon>
     <v-icon>mdi-office-building-outline</v-icon>
     <div id="companyIcon" class="mdi mdi-office-building-outline"></div>
-    <div id="test_text" style="font-family: 'Material Design Icons'"></div>
+    <div id="test_name" style="font-family: 'Material Design Icons'"></div>
     <div class="diagram-canvas" id="canvas01"></div>
   </div>
 </template>
@@ -20,7 +20,8 @@ import "jquery";
 import "jquery-ui";
 import "jquery-ui/ui/widgets/draggable";
 import "jquery-ui/ui/widgets/droppable";
-import draw2d from "draw2d";
+import draw2d, { Figure } from "draw2d";
+import { createWrapper } from "@vue/test-utils";
 
 @Component
 export default class extends Vue {
@@ -29,6 +30,8 @@ export default class extends Vue {
   public mounted() {
     this.showCanvas();
     this.fixCanvasPosition();
+
+    this.writeIconByCharCode();
   }
 
   private showCanvas(): void {
@@ -49,8 +52,132 @@ export default class extends Vue {
   }
 
   private addSampleObjects(canvas: draw2d.Canvas) {
+    this.wakuOnly(canvas);
+    this.groupingApproach(canvas);
+    this.layoutApproach(canvas);
+    this.structuredApproach2(canvas);
+
+    console.log(canvas);
+  }
+
+  private writeIconByCharCode(): void {
+    const div = document.getElementById("test_name");
+    if (!div) return;
+    div.innerHTML = "\u{F151F}";
+  }
+
+  /**
+   * Rectangleでは「透過にすると、枠線まで透過される」ので、中身透明な枠を作ることが出来ない。
+   * そこでStackLayoutに「透明のRectangle」を入れ、代用する。
+   */
+  private wakuOnly(canvas: draw2d.Canvas): void {
+    const onlyBordar = new draw2d.shape.layout.StackLayout();
+    const dummyRect = new draw2d.shape.basic.Rectangle({ alpha: 0 });
+    onlyBordar.add(dummyRect);
+    onlyBordar.setStroke(2);
+    onlyBordar.resizable = true;
+    onlyBordar.radius = 5;
+    onlyBordar.stroke = 3;
+    onlyBordar.width = 75;
+    onlyBordar.height = 75;
+    onlyBordar.color = new draw2d.util.Color(255, 0, 0);
+    // onlyBordar.bgColor = new draw2d.util.Color(0,255,0); // これも一応出来る(意味ないが)
+
+    canvas.add(onlyBordar, 50, 100);
+  }
+
+  private groupingApproach(canvas: draw2d.Canvas): void {
     const resourceId = 1;
-    const resourceName = "図書館";
+    const resourceName =
+      "グルーピングアプローチで作られた場合、ここに名前が出ますよ！";
+    const left = 200;
+    const top = 20;
+
+    // icon のTag(v-icon)から、フォントと文字を類推。
+    const iconTag = document.getElementById("companyIcon") as HTMLDivElement;
+    const style = window.getComputedStyle(iconTag, "::before");
+
+    const padding = 0;
+
+    const id = `${resourceId}-0`;
+    const waku = new draw2d.shape.basic.Rectangle({
+      x: left,
+      y: top + 27,
+      bgColor: "#FFFFFF",
+      alpha: 0.6, // opacityと一緒
+      width: 75,
+      height: 75,
+      radius: 5,
+      stroke: 3,
+      selectable: true,
+      resizable: true,
+      color: "#000000",
+      padding: padding,
+      id: id
+    });
+    canvas.add(waku);
+    const createdWaku = canvas.getFigure(id);
+
+    const id1 = `${resourceId}-1`;
+    const icon = new draw2d.shape.basic.Label({
+      x: left,
+      y: top,
+      fontFamily: style.fontFamily,
+      text: style.content.replace(/"/g, ""),
+      fontSize: 25,
+      stroke: 0,
+      padding: padding,
+      bgColor: "#FFFFFF",
+      alpha: 1, // opacityと一緒
+      id: id1
+    });
+
+    const id2 = `${resourceId}-2`;
+    const name = new draw2d.shape.basic.Label({
+      // x: left,
+      // y: top,
+      text: resourceName,
+      stroke: 0,
+      padding: padding,
+      resizable: false,
+      selectable: false,
+      id: id2
+    });
+    icon.add(name, new draw2d.layout.locator.RightLocator());
+    canvas.add(icon);
+    const createdIcon = canvas.getFigure(icon.id);
+
+    const groupItems = new draw2d.util.ArrayList();
+    groupItems.id = resourceId;
+    groupItems.add(createdWaku);
+    groupItems.add(createdIcon);
+    groupItems.resizable = true;
+    const groupCommand = new draw2d.command.CommandGroup(canvas, groupItems);
+    canvas.getCommandStack().execute(groupCommand);
+
+    const figures: Figure[] = canvas.getFigures() as Figure[];
+    const group = figures.find(
+      f => f.getCssClass() === "draw2d_shape_composite_Group"
+    );
+    if (!group) return;
+    // 下のほうほうでリサイズ可能にしても、グッシャグシャにバグる。
+    // group.setResizeable(true);
+    // group.resizeable = true;
+
+    // 自力でグループ作っても、上のようにならない。
+    // const group = new draw2d.shape.composite.Group();
+    // group.add(createdWaku, new draw2d.layout.locator.XYAbsPortLocator(0, 26));
+    // group.add(createdIcon, new draw2d.layout.locator.XYAbsPortLocator(20, 20));
+    // group.width = createdWaku.lastAppliedAttributes.width;
+    // group.height = createdWaku.lastAppliedAttributes.height + 26;
+    // canvas.add(group);
+    // const createdGroup = canvas.getFigure(group.id);
+    // createdGroup.resizable = true;
+  }
+
+  private layoutApproach(canvas: draw2d.Canvas): void {
+    const resourceId = 1;
+    const resourceName = "構造化アプローチ(Layout利用編)";
     const left = 20;
     const top = 20;
 
@@ -62,57 +189,112 @@ export default class extends Vue {
 
     const padding = 0;
 
-    const id3 = `${resourceId}-1`;
-    const icon = new draw2d.shape.basic.Label({
+    const id = resourceId + 2000000;
+    const waku = new draw2d.shape.basic.Rectangle({
       x: left,
       y: top,
-      //   width: 20,
-      //   height: 30,
+      bgColor: "#FFFFFF",
+      alpha: 0.6, // opacityと一緒
+      width: 75,
+      height: 75,
+      radius: 5,
+      stroke: 3,
+      // grow: true,
+      selectable: true,
+      resizable: true,
+      color: "#000000",
+      padding: padding
+    });
+
+    const icon = new draw2d.shape.basic.Label({
+      width: 20,
+      height: 30,
+
       fontFamily: style.fontFamily,
       text: style.content.replace(/"/g, ""),
       fontSize: 25,
       stroke: 0,
       padding: padding,
-      id: id3
+      bgColor: "#FFFFFF",
+      alpha: 1 // opacityと一緒
     });
-    canvas.add(icon);
-    const createdIcon = canvas.getFigure(id3);
-    console.log(createdIcon);
 
-    const id = `${resourceId}-2`;
-    const text = new draw2d.shape.basic.Label({
-      x: left + createdIcon.lastAppliedAttributes.width,
-      y: top + 6,
+    const name = new draw2d.shape.basic.Label({
       text: resourceName,
       stroke: 0,
       padding: padding,
-      id: id
+      resizable: false,
+      selectable: false
     });
-    canvas.add(text);
-    const createdText = canvas.getFigure(id);
-    console.log(createdText);
 
-    const id2 = `${resourceId}-3`;
+    const container = new draw2d.shape.layout.HorizontalLayout();
+
+    container.add(icon);
+    container.add(name);
+    container.setStroke(0);
+
+    waku.add(container, new draw2d.layout.locator.TopLocator());
+
+    canvas.add(waku);
+    const createdWaku = canvas.getFigure(id);
+  }
+
+  private structuredApproach2(canvas: draw2d.Canvas): void {
+    const resourceId = 1;
+    const resourceName = "構造化アプローチ(Locatorとadd利用編)";
+    const left = 500;
+    const top = 500;
+
+    // ---- icon作成 ----
+
+    // icon のTag(v-icon)から、フォントと文字を類推。
+    const iconTag = document.getElementById("companyIcon") as HTMLDivElement;
+    const style = window.getComputedStyle(iconTag, "::before");
+
+    const padding = 0;
+
+    const id = resourceId + 4000000;
     const waku = new draw2d.shape.basic.Rectangle({
       x: left,
-      y: top + createdIcon.lastAppliedAttributes.width + 5,
+      y: top,
       bgColor: "#FFFFFF",
       alpha: 0.6, // opacityと一緒
       width: 75,
       height: 75,
-      radius: 3,
+      radius: 5,
       stroke: 3,
-      grow: true,
+      selectable: true,
+      resizable: true,
       color: "#000000",
-      padding: padding,
-      id: id2
+      padding: padding
     });
-    canvas.add(waku);
-    const createdWaku = canvas.getFigure(id2);
 
-    const div = document.getElementById("test_text");
-    if (!div) return;
-    div.textContent = "\u{F151F}";
+    const icon = new draw2d.shape.basic.Label({
+      fontFamily: style.fontFamily,
+      text: style.content.replace(/"/g, ""),
+      fontSize: 25,
+      stroke: 0,
+      padding: padding,
+      bgColor: "#FFFFFF",
+      alpha: 1, // opacityと一緒
+    });
+
+    const name = new draw2d.shape.basic.Label({
+      text: resourceName,
+      stroke: 0,
+      padding: padding,
+      resizable: false,
+      selectable: false,
+    });
+
+    waku.add(icon, new draw2d.layout.locator.XYAbsPortLocator(40, 40));
+    waku.add(name, new draw2d.layout.locator.TopLocator());
+    // ↑狙ったのは「枠の中で、左上からの一定の位置をキープ」である…が左に張り付いてうんともすんともいわない。
+    // XYRelPortLocator(40, -40)だと反応するし、マイナスで枠外にも行くことから、バグのように感じられる。
+    // …しかし”PortLocator"だからなぁ。
+
+    canvas.add(waku);
+    const createdWaku = canvas.getFigure(id);
   }
 }
 </script>
@@ -121,7 +303,7 @@ export default class extends Vue {
 .diagram-canvas {
   width: 1024px;
   height: 768px;
-  background-color: white;
+  background-color: blue;
   margin: 0px;
 
   border-radius: 5px;
