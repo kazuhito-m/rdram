081af61a998c96075aad0e64098477e131955d81
kazuhito_m <kazuhito.sumpic@gmail.com>
2020-09-01 23:17:25 +0900

    implmenet: 神崎さんの動画みて「作成の順番」にフォルダを作り直し。

--- src/domain/diagram/DiagramType.ts
+++ src/domain/diagram/DiagramType.ts
@@ -1,18 +1,15 @@
-import Diagram from "./Diagram";
-import BusinessContextDiagram from "./businesscontext/BusinessContextDiagram";
-import ResourceType from "@/domain/resource/ResourceType";
-
 export default class DiagramType {
     private static readonly vs: DiagramType[] = [];
 
-    static ビジネスコンテキスト図 = new DiagramType(1, 'ビジネスコンテキスト図');
-    static システムコンテキスト図 = new DiagramType(2, 'システムコンテキスト図');
-    static 要求モデル図 = new DiagramType(3, '要求モデル図');
+    static システムコンテキスト図 = new DiagramType(1, 'システムコンテキスト図');
+    static 要求モデル図 = new DiagramType(2, '要求モデル図');
+    static ビジネスコンテキスト図 = new DiagramType(3, 'ビジネスコンテキスト図');
     static ビジネスユースケース図 = new DiagramType(4, 'ビジネスユースケース図');
-    static 情報モデル図 = new DiagramType(5, '情報モデル図');
-    static 状態モデル = new DiagramType(6, '状態モデル');
-    static ユースケース複合図 = new DiagramType(7, 'ユースケース複合図');
-    static バリエーション = new DiagramType(8, 'バリエーション');
+    static 利用シーン = new DiagramType(5, '利用シーン');
+    static 情報モデル図 = new DiagramType(6, '情報モデル図');
+    static 状態モデル = new DiagramType(7, '状態モデル');
+    static ユースケース複合図 = new DiagramType(8, 'ユースケース複合図');
+    static バリエーション = new DiagramType(9, 'バリエーション');
 
     private constructor(
         public readonly id: number,
