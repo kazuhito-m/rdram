25488786aba23f3028abab67e864aac8d3920b55
kazuhito_m <kazuhito.sumpic@gmail.com>
2020-09-01 23:25:22 +0900

    implement: オプショナルな図をコメントで追加。

--- src/domain/diagram/DiagramType.ts
+++ src/domain/diagram/DiagramType.ts
@@ -5,11 +5,12 @@ export default class DiagramType {
     static 要求モデル図 = new DiagramType(2, '要求モデル図');
     static ビジネスコンテキスト図 = new DiagramType(3, 'ビジネスコンテキスト図');
     static ビジネスユースケース図 = new DiagramType(4, 'ビジネスユースケース図');
-    static 利用シーン = new DiagramType(5, '利用シーン');
-    static 情報モデル図 = new DiagramType(6, '情報モデル図');
-    static 状態モデル = new DiagramType(7, '状態モデル');
-    static ユースケース複合図 = new DiagramType(8, 'ユースケース複合図');
-    static バリエーション = new DiagramType(9, 'バリエーション');
+    // static 業務フロー = new DiagramType(11, '業務フロー');
+    // static 利用シーン = new DiagramType(12, '利用シーン');
+    static 情報モデル図 = new DiagramType(5, '情報モデル図');
+    static 状態モデル = new DiagramType(6, '状態モデル');
+    static ユースケース複合図 = new DiagramType(7, 'ユースケース複合図');
+    static バリエーション = new DiagramType(8, 'バリエーション');
 
     private constructor(
         public readonly id: number,
