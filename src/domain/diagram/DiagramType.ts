470b2b3dce476baf07b57fee5ded5ef11a15cc78
kazuhito_m <kazuhito.sumpic@gmail.com>
2020-09-09 07:22:56 +0900

    implement: 業務フローのアイコンを設定。

--- src/domain/diagram/DiagramType.ts
+++ src/domain/diagram/DiagramType.ts
@@ -5,7 +5,7 @@ export default class DiagramType {
     static 要求モデル図 = new DiagramType(2, '要求モデル図', "mdi-alpha-r-box");
     static ビジネスコンテキスト図 = new DiagramType(3, 'ビジネスコンテキスト図', "mdi-alpha-c-box");
     static ビジネスユースケース図 = new DiagramType(4, 'ビジネスユースケース図', "mdi-alpha-u-box");
-    static 業務フロー図 = new DiagramType(11, '業務フロー図', "");
+    static 業務フロー図 = new DiagramType(11, '業務フロー図', "mdi-clipboard-flow");
     static 利用シーン図 = new DiagramType(12, '利用シーン図', "mdi-video-image");
     static 情報モデル図 = new DiagramType(5, '情報モデル図', "mdi-axis-arrow-info");
     static 状態モデル = new DiagramType(6, '状態モデル', "mdi-state-machine");
