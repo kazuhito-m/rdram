62cb65b7ef8ac7198af8025256bf5a815696ec5b
kazuhito_m <kazuhito.sumpic@gmail.com>
2021-10-31 05:08:40 +0900

    implement: ソースディレクトリを、一段落として動くか観てみる。

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,7 +84,7 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
-  loading: '~/presentation/components/Loading.vue',
+  loading: '~/components/Loading.vue',
   srcDir: 'src/',
 
   router: {
