e6259acf6af9c8d423543ad4d7598e0341742557
kazuhito_m <kazuhito.sumpic@gmail.com>
2021-12-12 06:16:35 +0900

    bugfix: Lintの指摘直し。

--- src/components/diagrams/editor/usecasecompositediagram/UseCaseCompositeDiagramEditor.vue
+++ src/components/diagrams/editor/usecasecompositediagram/UseCaseCompositeDiagramEditor.vue
@@ -46,8 +46,10 @@ export default class UseCaseCompositeDiagramEditor extends Vue {
 
   @Prop({ required: true })
   private readonly diagramId!: number;
+
   @Prop({ required: true })
   private readonly allResourcesOnCurrentProduct!: Resource[];
+
   @Prop({ required: true })
   private readonly lastPropertiesUpdatedDiagramId?: number;
 
@@ -81,7 +83,7 @@ export default class UseCaseCompositeDiagramEditor extends Vue {
   private onUpdateResources(): void {}
 
   @Emit("onOpendDiagramPropertiesEditor")
-  private onOpendDiagramPropertiesEditor(diagramId: number): void {}
+  private onOpendDiagramPropertiesEditor(_diagramId: number): void {}
 }
 </script>
 
