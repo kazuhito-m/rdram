165c2ea13809a37d8b34b6f37920547176e4467c
kazuhito_m <kazuhito.sumpic@gmail.com>
2021-12-12 06:01:39 +0900

    bugfix: Lintの指摘直し。

--- src/components/diagrams/editor/toolbar/CanvasGuideType.ts
+++ src/components/diagrams/editor/toolbar/CanvasGuideType.ts
@@ -1,11 +1,11 @@
 import draw2d from "draw2d";
 
 export default class CanvasGuideType {
-    private static readonly vs: CanvasGuideType[] = [];
-
     static なし = new CanvasGuideType(1, "mdi-grid-off", "なし(白地)", null);
     static グリッド = new CanvasGuideType(2, "mdi-grid", "グリッド(方眼・格子状)", new draw2d.policy.canvas.SnapToGridEditPolicy());
     static ジオメトリ = new CanvasGuideType(3, "mdi-crop", "ジオメトリ(他アイコン位置をガイド)", new draw2d.policy.canvas.SnapToGeometryEditPolicy());
+
+    private static readonly vs: any[] = [];
 
     private constructor(
         public readonly id: number,
@@ -25,15 +25,17 @@ export default class CanvasGuideType {
         return CanvasGuideType.vs;
     }
 
-    public static ofId(id: number): CanvasGuideType | null {
+    public static ofId(id: number): CanvasGuideType {
         const found = this.vs
             .find(item => item.id === id);
-        return found ? found : CanvasGuideType.なし;
+        if (found) return found;
+        return CanvasGuideType.なし;
     }
 
-    public static ofIconKey(iconKey: string): CanvasGuideType | null {
+    public static ofIconKey(iconKey: string): CanvasGuideType {
         const found = this.vs
             .find(item => item.iconKey === iconKey);
-        return found ? found : CanvasGuideType.なし;
+        if (found) return found;
+        return CanvasGuideType.なし;
     }
 }
