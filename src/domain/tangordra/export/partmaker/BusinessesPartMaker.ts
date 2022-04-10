3aa75824268b21746824fddf258514ef2a30b584
kazuhito-m <kazuhito.sumpic@gmail.com>
2022-04-10 11:15:00 +0900

    implement: Business[] を作る「おおまかな流れ」を作る。

--- src/domain/tangordra/export/partmaker/BusinessesPartMaker.ts
+++ src/domain/tangordra/export/partmaker/BusinessesPartMaker.ts
@@ -1,8 +1,43 @@
-import { Business } from "../structure/TangoRdra";
-import Product from "~/domain/product/Product";
+import { BUC, Business } from "../structure/TangoRdra";
+import Product from "@/domain/product/Product";
+import DiagramType from "@/domain/diagram/DiagramType";
+import Diagram from "@/domain/diagram/Diagram";
+import Resources from "@/domain/resource/Resources";
+import Resource from "@/domain/resource/Resource";
+import ResourceType from "@/domain/resource/ResourceType";
 
 export default class BusinessesPartMaker {
     public make(product: Product): Business[] {
-        return [];
+        return product.diagrams
+            .typeOf(DiagramType.ビジネスユースケース図)
+            .map(diagram => this.makeBusiness(diagram, product));
+    }
+
+    private makeBusiness(diagram: Diagram,  product: Product): Business {
+        const allResouses = product.resources;
+        const maybeUse = diagram.placements
+            .map(placement => allResouses.of(placement.resourceId))
+            .filter(resource => resource !== undefined) as Resource[];
+        const useResources = new Resources(maybeUse);
+
+        const result =  {
+            name: diagram.name,
+        } as Business;
+
+        const actorNames = useResources.typeOf(ResourceType.アクター)
+            .map(actor => actor.name);
+        if (actorNames.length > 0) result.main_actor = actorNames;
+
+        const bucs = useResources.typeOf(ResourceType.ビジネスユースケース)
+            .map(bucResource => this.makeBucs(bucResource, product));
+        if (bucs.length > 0) result.buc = bucs;
+
+        return result;
+    }
+
+    private makeBucs(buc: Resource, product: Product): BUC {
+        return {
+            name: buc.name
+        } as BUC;
     }
 }
