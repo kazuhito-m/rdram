6493812b63d068a9a29454461756c012f2092072
kazuhito-m <kazuhito.sumpic@gmail.com>
2022-04-12 03:15:00 +0900

    implement: Usacaseの出力を一旦の完成。

--- src/domain/tangordra/export/partmaker/UsecasesPartMaker.ts
+++ src/domain/tangordra/export/partmaker/UsecasesPartMaker.ts
@@ -5,12 +5,13 @@ import Diagram from "~/domain/diagram/Diagram";
 import ResourceType from "~/domain/resource/ResourceType";
 import Resources from "~/domain/resource/Resources";
 import Resource from "~/domain/resource/Resource";
+import Relation from "~/domain/relation/Relation";
 
 export default class UsecasesPartMaker {
     public make(product: Product): Usecase[] {
         const usecases = product.resources
             .typeOf(ResourceType.ユースケース);
-        const maybeRelatedResources = product.resources
+        const allTargetResources = product.resources
             .typesOf(ResourceType.タイマー
                 , ResourceType.画面
                 , ResourceType.情報
@@ -19,16 +20,44 @@ export default class UsecasesPartMaker {
             );
 
         return usecases
-            .map(usecase => this.makeUsecase(usecase, maybeRelatedResources, product));
+            .map(usecase => this.makeUsecase(usecase, allTargetResources, product));
     }
 
-    private makeUsecase(usecase: Resource, maybeRelatedResources: Resources, product: Product): Usecase {
+    private makeUsecase(usecase: Resource, allTargetResources: Resources, product: Product): Usecase {
         const result = {
-            name: usecase.name           
+            name: usecase.name
         } as Usecase;
 
-        // TODO Usecase一つ作る実装。
+        const maybeRelatedResources = product.diagrams
+            .typeOf(DiagramType.ユースケース複合図)
+            .allRelations()
+            .uniqueIgnoreDirection()
+            .filter(relation => relation.isRelatedTo(usecase.resourceId))
+            .map(relation => relation.otherSideOf(usecase.resourceId))
+            .filter(otherSideResourceId => allTargetResources.existsIdOf(otherSideResourceId))
+            .map(otherSideResourceId => allTargetResources.of(otherSideResourceId));
+        const relatedResources = new Resources(maybeRelatedResources as Resource[]);
+
+        const timerNames = this.findNamesOf(relatedResources, ResourceType.タイマー);
+        if (timerNames.length > 0) result.timer = timerNames;
+
+        const viewNames = this.findNamesOf(relatedResources, ResourceType.画面);
+        if (viewNames.length > 0) result.view = viewNames;
+
+        const informationNames = this.findNamesOf(relatedResources, ResourceType.情報);
+        if (informationNames.length > 0) result.information = informationNames;
+
+        const conditionNames = this.findNamesOf(relatedResources, ResourceType.条件, ResourceType.表形式の条件);
+        if (conditionNames.length > 0) result.condition = conditionNames;
+
+        const eventNames = this.findNamesOf(relatedResources, ResourceType.イベント);
+        if (eventNames.length > 0) result.event = eventNames;
 
         return result;
+    }
+
+    private findNamesOf(useRecources: Resources, ...resourceTypes: ResourceType[]): string[] {
+        return useRecources.typesOf(...resourceTypes)
+            .map(resource => resource.name);
     }
 }
