6d069a5bd253b4e94495e8866ce73ea900386ff4
kazuhito-m <kazuhito.sumpic@gmail.com>
2022-04-12 01:21:41 +0900

    implement: Usecaseのメソッドシグネチャだけ定義。

--- src/domain/tangordra/export/partmaker/UsecasesPartMaker.ts
+++ src/domain/tangordra/export/partmaker/UsecasesPartMaker.ts
@@ -1,8 +1,22 @@
 import { Usecase } from "../structure/TangoRdra";
 import Product from "~/domain/product/Product";
+import DiagramType from "~/domain/diagram/DiagramType";
+import Diagram from "~/domain/diagram/Diagram";
 
 export default class UsecasesPartMaker {
     public make(product: Product): Usecase[] {
-        return [];
+        return product.diagrams
+            .typeOf(DiagramType.ユースケース複合図)
+            .map(diagram => this.makeUsecase(diagram, product));
+    }
+
+    private makeUsecase(diagram: Diagram, product: Product): Usecase {
+        const result = {
+            name: diagram.name
+        } as Usecase;
+
+        // TODO パーツ実装
+
+        return result;
     }
 }
