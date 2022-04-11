d3b27b093a5dee06dc2314a79fa25210a1351cf6
kazuhito-m <kazuhito.sumpic@gmail.com>
2022-04-12 02:02:00 +0900

    implement: 実装が完全に間違っていた。一旦もとに戻す

--- src/domain/tangordra/export/partmaker/UsecasesPartMaker.ts
+++ src/domain/tangordra/export/partmaker/UsecasesPartMaker.ts
@@ -7,38 +7,6 @@ import Resources from "~/domain/resource/Resources";
 
 export default class UsecasesPartMaker {
     public make(product: Product): Usecase[] {
-        return product.diagrams
-            .typeOf(DiagramType.ユースケース複合図)
-            .map(diagram => this.makeUsecase(diagram, product));
-    }
-
-    private makeUsecase(diagram: Diagram, product: Product): Usecase {
-        const result = {
-            name: diagram.name
-        } as Usecase;
-
-        const useRecources = product.useResourcesOf(diagram);
-
-        const timerNames = this.findNamesOf(useRecources, ResourceType.タイマー);
-        if (timerNames.length > 0) result.timer = timerNames;
-
-        const viewNames = this.findNamesOf(useRecources, ResourceType.画面);
-        if (viewNames.length > 0) result.view = viewNames;
-
-        const informationNames = this.findNamesOf(useRecources, ResourceType.情報);
-        if (informationNames.length > 0) result.information = informationNames;
-
-        const conditionNames = this.findNamesOf(useRecources, ResourceType.条件, ResourceType.表形式の条件);
-        if (conditionNames.length > 0) result.condition = conditionNames;
-
-        const eventNames = this.findNamesOf(useRecources, ResourceType.イベント);
-        if (eventNames.length > 0) result.event = eventNames;
-
-        return result;
-    }
-
-    private findNamesOf(useRecources: Resources, ...resourceTypes: ResourceType[]): string[] {
-        return useRecources.typesOf(...resourceTypes)
-            .map(resource => resource.name);
+       return [];
     }
 }
