44b6e87e89f32ce53082a26412c19ef4abd3dcbd
kazuhito-m <kazuhito.sumpic@gmail.com>
2022-04-12 01:55:54 +0900

    implement: usecaseの出力を実装。

--- src/domain/tangordra/export/partmaker/UsecasesPartMaker.ts
+++ src/domain/tangordra/export/partmaker/UsecasesPartMaker.ts
@@ -2,6 +2,8 @@ import { Usecase } from "../structure/TangoRdra";
 import Product from "~/domain/product/Product";
 import DiagramType from "~/domain/diagram/DiagramType";
 import Diagram from "~/domain/diagram/Diagram";
+import ResourceType from "~/domain/resource/ResourceType";
+import Resources from "~/domain/resource/Resources";
 
 export default class UsecasesPartMaker {
     public make(product: Product): Usecase[] {
@@ -15,8 +17,28 @@ export default class UsecasesPartMaker {
             name: diagram.name
         } as Usecase;
 
-        // TODO パーツ実装
+        const useRecources = product.useResourcesOf(diagram);
+
+        const timerNames = this.findNamesOf(useRecources, ResourceType.タイマー);
+        if (timerNames.length > 0) result.timer = timerNames;
+
+        const viewNames = this.findNamesOf(useRecources, ResourceType.画面);
+        if (viewNames.length > 0) result.view = viewNames;
+
+        const informationNames = this.findNamesOf(useRecources, ResourceType.情報);
+        if (informationNames.length > 0) result.information = informationNames;
+
+        const conditionNames = this.findNamesOf(useRecources, ResourceType.条件, ResourceType.表形式の条件);
+        if (conditionNames.length > 0) result.condition = conditionNames;
+
+        const eventNames = this.findNamesOf(useRecources, ResourceType.イベント);
+        if (eventNames.length > 0) result.event = eventNames;
 
         return result;
+    }
+
+    private findNamesOf(useRecources: Resources, ...resourceTypes: ResourceType[]): string[] {
+        return useRecources.typesOf(...resourceTypes)
+            .map(resource => resource.name);
     }
 }
