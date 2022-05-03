58dc62ae22e44a0fb662577a832511b910893f1d
kazuhito-m <kazuhito.sumpic@gmail.com>
2022-05-04 04:54:48 +0900

    test: ひとまず！動くテストを実装。

--- test/application/service/storage/import/LocalStorageImportService.spec.ts
+++ test/application/service/storage/import/LocalStorageImportService.spec.ts
@@ -8,19 +8,22 @@ describe('LocalStorageImportService', () => {
   test('中身がないなど、無効なファイルを指定された場合エラーを通知する。', async () => {
     const sut = createSut();
 
-    const file = {
-      name: "rdram-localstrage-0.json",
-      text : async (): Promise<string> => {
-        return "";
-      }
-    } as File;
+    const file = new File([""], 
+      "rdram-localstorage-backup-0.json",
+      { type: 'text/html' }
+    );
 
-    let resultEvent!: LocalStorageImportProgressEvent;
-    const actual = await sut.importOf(file,
-      event => resultEvent = event);
+    let lastError!: LocalStorageImportError;
+    const callback = async (event: LocalStorageImportProgressEvent): Promise<void> => {
+      if (event.error === LocalStorageImportError.なし) return;
+
+      console.log('callbackがコールされた。', 'step', event.step, 'error', event.error);
+      lastError = event.error;
+    }
+    const actual = await sut.importOf(file, callback);
 
     expect(actual).toBeNull();
-    expect(resultEvent.error).toEqual(LocalStorageImportError.非JSON形式);
+    expect(lastError).toEqual(LocalStorageImportError.非JSON形式);
   });
 })
 
