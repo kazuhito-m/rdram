c35ecc3d8a2b753a856a72e236459e7115c26b1a
kazuhito-m <kazuhito.sumpic@gmail.com>
2022-05-04 05:19:48 +0900

    code reformat.

--- test/application/service/storage/import/LocalStorageImportService.spec.ts
+++ test/application/service/storage/import/LocalStorageImportService.spec.ts
@@ -1,7 +1,7 @@
 import LocalStorageImportService from "@/application/service/storage/import/LocalStorageImportService";
-import { LocalStorageImportError } from "~/domain/storage/import/LocalStorageImportError";
-import FileSystemDatasouce from "~/infrastructure/filesystem/FileSystemDatasource";
-import StorageDatasource from "~/infrastructure/storage/StorageDatasource";
+import { LocalStorageImportError } from "@/domain/storage/import/LocalStorageImportError";
+import FileSystemDatasouce from "@/infrastructure/filesystem/FileSystemDatasource";
+import StorageDatasource from "@/infrastructure/storage/StorageDatasource";
 
 describe('LocalStorageImportService', () => {
   const sut = new LocalStorageImportService(new StorageDatasource(), new FileSystemDatasouce());
