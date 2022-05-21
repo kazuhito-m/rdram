8a6e2f2337a9fdbf348576f949faa699d4b70695
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-05-22 06:05:04 +0900

    implement: 大まかなレイアウト調整。
    デバッグで背景を白に。

--- src/components/diagrams/import/arrange/UserArrengeWhenNameConfrictDialog.vue
+++ src/components/diagrams/import/arrange/UserArrengeWhenNameConfrictDialog.vue
@@ -1,24 +1,47 @@
 <template lang="html">
-  <v-dialog v-model="visible" persistent width="650" height="500">
+  <v-dialog v-model="visible" persistent width="800" height="600" light>
     <v-card>
       <v-card-title class="headline">
         <v-icon>mdi-sync-alert</v-icon> 図/アイコンの名前の重複
       </v-card-title>
       <v-card-text>
-        インポートした内容に、名前の重複がありました。<br />
-        どのように扱うかを指定してください。
+        インポートした内容に「名前の重複」がありました。どのように扱うかを指定してください。
       </v-card-text>
       <v-card-text>
         <v-row>
-          <v-col cols="12" md="12">
-            <v-card>
-              <v-text-field label="インポートされた図の名前" filled readonly>
-              </v-text-field>
-              <v-card>
-                <v-card-text>
-                  <v-card-text> aaa </v-card-text>
-                </v-card-text>
-              </v-card>
+          <v-col cols="12" md="12" class="py-1">
+            <v-card outlined class="pa-1">
+              <v-card-subtitle class="pa-0">インポートした図</v-card-subtitle>
+              <v-card-text class="pa-1">
+                <v-text-field
+                  label="図の名前"
+                  dense
+                  hide-details
+                  filled
+                  readonly
+                  disabled
+                />
+                <v-card outlined class="pa-0">
+                  <v-card-text class="py-0 px-2">
+                    <v-radio-group row class="pa-0">
+                      <v-radio label="上書" value="radio-1"></v-radio>
+                      <v-radio label="別名" value="radio-2"></v-radio>
+                      <v-text-field
+                        label="新しい名前"
+                        dense
+                        hide-details
+                      ></v-text-field>
+                    </v-radio-group>
+                  </v-card-text>
+                </v-card>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="12" class="py-1">
+            <v-card outlined class="pa-1">
+              <v-card-subtitle class="pa-0">インポートしたアイコン</v-card-subtitle>
             </v-card>
           </v-col>
         </v-row>
