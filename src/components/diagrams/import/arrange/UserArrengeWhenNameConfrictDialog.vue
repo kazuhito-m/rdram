19f693ccedddda4846a5191f8b072909fcbad3c6
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-05-22 04:10:25 +0900

    implement: 通知ダイアログのデザインを決める。

--- src/components/diagrams/import/arrange/UserArrengeWhenNameConfrictDialog.vue
+++ src/components/diagrams/import/arrange/UserArrengeWhenNameConfrictDialog.vue
@@ -1,14 +1,32 @@
 <template lang="html">
-  <v-dialog v-model="visible" persistent width="400" height="400">
+  <v-dialog v-model="visible" persistent width="650" height="500">
     <v-card>
-      <v-toolbar dark dense flat>
-        <v-toolbar-title class="white--text">タイトル</v-toolbar-title>
-      </v-toolbar>
-      <v-card-text>ここに本文</v-card-text>
-      <v-card-actions class="pt-0">
+      <v-card-title class="headline">
+        <v-icon>mdi-sync-alert</v-icon> 図/アイコンの名前の重複
+      </v-card-title>
+      <v-card-text>
+        インポートした内容に、名前の重複がありました。<br />
+        どのように扱うかを指定してください。
+      </v-card-text>
+      <v-card-text>
+        <v-row>
+          <v-col cols="12" md="12">
+            <v-card>
+              <v-text-field label="インポートされた図の名前" filled readonly>
+              </v-text-field>
+              <v-card>
+                <v-card-text>
+                  <v-card-text> aaa </v-card-text>
+                </v-card-text>
+              </v-card>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="primary darken-1" @click.native="onOkClick">OK</v-btn>
-        <v-btn color="grey" @click.native="onClickCancel">Cancel</v-btn>
+        <v-btn text color="normal" @click="onClickCancel"> キャンセル </v-btn>
+        <v-btn text color="green darken-1" @click="onOkClick"> 実行 </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
