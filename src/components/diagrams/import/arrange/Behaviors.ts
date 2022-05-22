f1c828cc8cd80f4b072ec25f781f7bcc3be83d99
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-05-23 03:44:04 +0900

    implement: Resourceが重複した際の説明文を定義した。 　

--- src/components/diagrams/import/arrange/Behaviors.ts
+++ src/components/diagrams/import/arrange/Behaviors.ts
@@ -18,14 +18,24 @@ export default class Behaviors {
         .map(value => new Behavior(
             value,
             this.nameOf(value),
-            this.descriptionOfDiagramNameOf(value)
+            this.descriptionOfResourceNameOf(value)
         ));
 
     private descriptionOfDiagramNameOf(value: BehaviorWhenNameConflict): string {
         if (value === BehaviorWhenNameConflict.置換)
-            return "";
+            return "既にある「同名の図」をインポート内容で上書き・置き換えます。";
         if (value === BehaviorWhenNameConflict.別名)
-            return "";
+            return "入力した「新しい名前」でインポート内容の図を作成します。";
+        return "";
+    }
+
+    private descriptionOfResourceNameOf(value: BehaviorWhenNameConflict): string {
+        if (value === BehaviorWhenNameConflict.既存)
+            return "インポートした図のアイコンは使わず、既にある「同名のアイコン」を使います。";
+        if (value === BehaviorWhenNameConflict.置換)
+            return "既にある「同名のアイコン」をインポート内容で上書き・置き換えます。";
+        if (value === BehaviorWhenNameConflict.別名)
+            return "入力した「新しい名前」でインポート内容のアイコンを作成します。";
         return "";
     }
 
