6dbcb8ea597d050e6bc9667e113c041b74c45ad3
kazuhito-m <kazuhito-m@users.noreply.github.com>
2022-06-03 10:38:03 +0000

    deploy: fbe25c8881ffd34361afd6390670ec9ec7f2be28

---
+++ _nuxt/8d9f930.js
@@ -0,0 +1,30 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[96],{
+
+/***/ 946:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(8);
+/* harmony import */ var vuetify_lib__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(220);
+
+
+vue__WEBPACK_IMPORTED_MODULE_0__["default"].use(vuetify_lib__WEBPACK_IMPORTED_MODULE_1__[/* default */ "a"]);
+/**
+ * 本システムのアイコンのエイリアス。
+ *
+ * TODO が、動かない。いつかうごかせるようにする。
+ */
+
+/* harmony default export */ __webpack_exports__["default"] = (new vuetify_lib__WEBPACK_IMPORTED_MODULE_1__[/* default */ "a"]({
+  icons: {
+    iconfont: 'mdi',
+    values: {
+      company: 'mdi-office-building-outline'
+    }
+  }
+}));
+
+/***/ })
+
+}]);
