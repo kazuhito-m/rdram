e126860ab78109a54747bf1047b38b862986c528
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-06-26 12:52:56 +0900

    refactor: 性質を抽出スべく、Abstructクラスを作成。

---
+++ src/components/twopain/DragAndDropValue.ts
@@ -0,0 +1,44 @@
+export default abstract class DragAndDropValue<T> {
+    constructor(
+        private readonly type: string,
+        private readonly value: string
+    ) { }
+
+    protected abstract typeName(): string;
+
+    protected abstract convIdToValue(id: T): string ;
+
+    protected abstract convValueToId(value: string): T ;
+
+    protected abstract of(type: string, value: string): DragAndDropValue<T> ;
+
+    protected abstract empty(): DragAndDropValue<T> ;
+
+    is(): boolean {
+        return this.type === this.typeName();
+    }
+
+    isInvalid(): boolean {
+        return !this.is() || this.value === "";
+    }
+
+    id(): T {
+        return this.convValueToId(this.value);
+    }
+
+    toString(): string {
+        return `${this.type}:${this.value}`;
+    }
+
+    parseOf(text?: string): DragAndDropValue<T> {
+        if (!text) return this.empty();
+        const parts = text.split(":");
+        const value = parts[1];
+        if (value.length === 0) return this.empty();
+        return this.of(parts[0], value)
+    }
+
+    renew(slideBarId: T): DragAndDropValue<T> {
+        return this.of(this.typeName(), this.convIdToValue(slideBarId));
+    }
+}
