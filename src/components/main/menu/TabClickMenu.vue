8d4b586cd0f9be88fb80478f4e4c723c49dfedec
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-06-27 01:08:47 +0900

    implement: メニューの有効無効を設定する。

--- src/components/main/menu/TabClickMenu.vue
+++ src/components/main/menu/TabClickMenu.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
     <v-list dence>
-      <v-list-item link @click="onClickCloseTab">
-        <v-list-item-title>他のタブを全て閉じる</v-list-item-title>
+      <v-list-item v-if="visibleOtherClose" @click="onClickCloseTab" link>
+        <v-list-item-title>他のタブをすべて閉じる</v-list-item-title>
       </v-list-item>
-      <v-list-item link @click="onClickCloseTab">
-        <v-list-item-title>左のタブの全てを閉じる</v-list-item-title>
+      <v-list-item v-if="visibleLeftClose" @click="onClickCloseTab" link>
+        <v-list-item-title>左のタブをすべて閉じる</v-list-item-title>
       </v-list-item>
-      <v-list-item link @click="onClickCloseTab">
-        <v-list-item-title>右のタブの全てを閉じる</v-list-item-title>
+      <v-list-item v-if="visibleRightClose" @click="onClickCloseTab" link>
+        <v-list-item-title>右のタブをすべて閉じる</v-list-item-title>
       </v-list-item>
-      <v-list-item link @click="onClickCloseTab">
-        <v-list-item-title>タブを全て閉じる</v-list-item-title>
+      <v-list-item @click="onClickCloseTab" link>
+        <v-list-item-title>タブをすべて閉じる</v-list-item-title>
       </v-list-item>
     </v-list>
   </div>
@@ -27,6 +27,22 @@ export default class TabClickMenu extends Vue {
 
   @Prop({ required: true })
   readonly openTabs!: ViewOrFolder[]
+
+  get visibleOtherClose(): boolean {
+    return this.visibleLeftClose && this.visibleRightClose
+  }
+
+  get visibleLeftClose(): boolean {
+    const tabs = this.openTabs
+    if (tabs.length < 2) return false
+    return !tabs[0].equals(this.item)
+  }
+
+  get visibleRightClose(): boolean {
+    const tabs = this.openTabs
+    if (tabs.length < 2) return false
+    return !tabs[tabs.length - 1].equals(this.item)
+  }
 
   onClickCloseTab(): void {
     alert('閉じクリ。tab:' + this.item.id + ', タブ数:' + this.openTabs.length)
