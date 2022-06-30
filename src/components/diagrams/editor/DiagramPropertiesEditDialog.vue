6e25eba9ed9bd99d5d602c33e5743d39a0d69f09
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-07-01 02:41:57 +0900

    code reformat.

--- src/components/diagrams/editor/DiagramPropertiesEditDialog.vue
+++ src/components/diagrams/editor/DiagramPropertiesEditDialog.vue
@@ -56,155 +56,154 @@ import {
   Prop,
   Inject,
   Emit,
-  Watch
-} from "nuxt-property-decorator";
-import PropertiesSettingDialog from "@/components/PropertiesSettingDialog.vue";
-import StorageRepository from "@/domain/storage/StorageRepository";
-import Diagram from "@/domain/diagram/Diagram";
-import Product from "@/domain/product/Product";
+  Watch,
+} from 'nuxt-property-decorator'
+import PropertiesSettingDialog from '@/components/PropertiesSettingDialog.vue'
+import StorageRepository from '@/domain/storage/StorageRepository'
+import Diagram from '@/domain/diagram/Diagram'
+import Product from '@/domain/product/Product'
 
 @Component({
-  components: { PropertiesSettingDialog }
+  components: { PropertiesSettingDialog },
 })
 export default class DiagramPropertiesEditDialog extends Vue {
   @Prop({ required: true })
-  readonly diagramId!: number;
+  readonly diagramId!: number
 
-  @Emit("onUpdatedDiagramProperties")
+  @Emit('onUpdatedDiagramProperties')
   private onUpdatedDiagramProperties(_diagram: Diagram): void {}
 
-  @Emit("onClose")
+  @Emit('onClose')
   onClose(): void {}
 
-  @Watch("diagramId")
+  @Watch('diagramId')
   onChangeDiagramId(): void {
-    if (!Number.isInteger(this.diagramId)) return;
-    const id = Number(this.diagramId);
-    if (id > 0) this.onShow();
+    if (!Number.isInteger(this.diagramId)) return
+    const id = Number(this.diagramId)
+    if (id > 0) this.onShow()
   }
 
   @Inject()
-  private repository?: StorageRepository;
+  private repository?: StorageRepository
 
-  consent = false;
-  subTitle = "";
-  title = "";
-  iconKey = "";
-  old!: Diagram;
+  consent = false
+  subTitle = ''
+  title = ''
+  iconKey = ''
+  old!: Diagram
 
-  name = "";
-  width = "";
-  height = "";
+  name = ''
+  width = ''
+  height = ''
 
   onShow(): void {
-    this.consent = false;
-    const product = this.repository?.getCurrentProduct();
-    const diagram = product?.diagrams.of(this.diagramId);
-    if (!diagram) return;
-    this.old = diagram;
-    this.title = `${diagram.name} の設定`;
-    this.subTitle = diagram.type.name;
-    this.iconKey = diagram.type.iconKey;
-    this.showProperties(diagram);
+    this.consent = false
+    const product = this.repository?.getCurrentProduct()
+    const diagram = product?.diagrams.of(this.diagramId)
+    if (!diagram) return
+    this.old = diagram
+    this.title = `${diagram.name} の設定`
+    this.subTitle = diagram.type.name
+    this.iconKey = diagram.type.iconKey
+    this.showProperties(diagram)
   }
 
   private showProperties(diagram: Diagram): void {
-    this.name = diagram.name;
-    this.width = diagram.width.toString();
-    this.height = diagram.height.toString();
+    this.name = diagram.name
+    this.width = diagram.width.toString()
+    this.height = diagram.height.toString()
   }
 
   private changed(): boolean {
-    const old = this.old!;
+    const old = this.old!
     return (
       old.name !== this.name ||
       old.width !== this.getWidth() ||
       old.height !== this.getHeight()
-    );
+    )
   }
 
   get nameMaxLength(): number {
-    return Diagram.NAME_MAX_LENGTH;
+    return Diagram.NAME_MAX_LENGTH
   }
 
   getWidth(): number {
-    return Number(this.width);
+    return Number(this.width)
   }
 
   getHeight(): number {
-    return Number(this.height);
+    return Number(this.height)
   }
 
   validateName(): string | boolean {
-    this.consent = false;
-    const name = this.name;
-    if (name.length === 0) return "入力してください。";
-    const max = this.nameMaxLength;
-    if (name.length > max) return `${max}文字以内で入力してください。`;
-    this.consent = this.changed();
-    return true;
+    this.consent = false
+    const name = this.name
+    if (name.length === 0) return '入力してください。'
+    const max = this.nameMaxLength
+    if (name.length > max) return `${max}文字以内で入力してください。`
+    this.consent = this.changed()
+    return true
   }
 
   validateWidith(): string | boolean {
-    return this.validateSize(this.width, Diagram.MAX_WIDTH);
+    return this.validateSize(this.width, Diagram.MAX_WIDTH)
   }
 
   validateHeight(): string | boolean {
-    return this.validateSize(this.height, Diagram.MAX_HEIGHT);
+    return this.validateSize(this.height, Diagram.MAX_HEIGHT)
   }
 
   private validateSize(value: string, max: number): string | boolean {
-    this.consent = false;
-    if (!value) return "入力してください。";
-    if (!Number.isInteger(Number(value))) return "数値を入力して下さい。";
-    const widthNumber = Number(value);
-    const min = 1;
-    if (widthNumber < min) return `${min} 以上で入力してください。`;
-    if (widthNumber > max) return `${max} 以下で入力してください。`;
-    this.consent = this.changed();
-    return true;
+    this.consent = false
+    if (!value) return '入力してください。'
+    if (!Number.isInteger(Number(value))) return '数値を入力して下さい。'
+    const widthNumber = Number(value)
+    const min = 1
+    if (widthNumber < min) return `${min} 以上で入力してください。`
+    if (widthNumber > max) return `${max} 以下で入力してください。`
+    this.consent = this.changed()
+    return true
   }
 
   onClickUpdateExecute(): void {
-    if (!this.consent) return;
-    const diagram = this.registerDiagramProperties();
-    if (!diagram) return;
-    this.onUpdatedDiagramProperties(diagram);
-    this.onClose();
+    if (!this.consent) return
+    const diagram = this.registerDiagramProperties()
+    if (!diagram) return
+    this.onUpdatedDiagramProperties(diagram)
+    this.onClose()
   }
 
   private registerDiagramProperties(): Diagram | null {
-    const product = this.repository?.getCurrentProduct();
-    const diagram = product?.diagrams.of(this.diagramId);
-    if (!product || !diagram) return null;
+    const product = this.repository?.getCurrentProduct()
+    const diagram = product?.diagrams.of(this.diagramId)
+    if (!product || !diagram) return null
 
     const modified = diagram
       .renameOf(this.name)
-      .resize(this.getWidth(), this.getHeight());
-    if (!this.logicalValidation(modified, product)) return null;
+      .resize(this.getWidth(), this.getHeight())
+    if (!this.logicalValidation(modified, product)) return null
 
-    const registerd = modified.fixStickOuts();
-    const modifiedProduct = product.meageDiagramByIdOf(registerd);
-    this.repository?.registerCurrentProduct(modifiedProduct);
+    const registerd = modified.fixStickOuts()
+    const modifiedProduct = product.meageDiagramByIdOf(registerd)
+    this.repository?.registerCurrentProduct(modifiedProduct)
 
-    return registerd;
+    return registerd
   }
 
   private logicalValidation(diagram: Diagram, product: Product): boolean {
     if (product.diagrams.existsSameOf(diagram)) {
-      alert("既に重複した名前の図が在ります。");
-      return false;
+      alert('既に重複した名前の図が在ります。')
+      return false
     }
     if (diagram.existsStickOutPlacements()) {
       const message =
-        "このサイズ変更を実行すると、はみ出すアイコンがあります。\n" +
-        "はみ出したアイコンは、実行時に削除されます。\nよろしいですか。";
-      if (!window.confirm(message)) return false;
+        'このサイズ変更を実行すると、はみ出すアイコンがあります。\n' +
+        'はみ出したアイコンは、実行時に削除されます。\nよろしいですか。'
+      if (!window.confirm(message)) return false
     }
-    return true;
+    return true
   }
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
