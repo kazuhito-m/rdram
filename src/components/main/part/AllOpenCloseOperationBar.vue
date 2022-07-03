21fbe270140a8a29d689b69f8b9b6411bf7aef98
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-07-04 02:14:13 +0900

    implement: バーを用意する。

---
+++ src/components/main/part/AllOpenCloseOperationBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card light class="test">
+    <v-card-actions>
+      <v-spacer />
+      <v-btn x-small icon>
+        <v-icon small>mdi-plus-box-multiple</v-icon>
+      </v-btn>
+      <v-btn x-small icon>
+        <v-icon small>mdi-minus-box-multiple</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Emit, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class AllOpenCloseOperationBar extends Vue {
+
+}
+</script>
+
+<style scoped>
+.v-card__actions {
+  padding-left: 8px;
+  padding-right: 8px;
+  padding-top: 1px;
+  padding-bottom: 1px;
+}
+</style>
