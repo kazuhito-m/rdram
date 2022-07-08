c870752a584f2658bdfe1a05c1bd43654207b0d1
kazuhito_m <kazuhito.sumpic@gmail.com>
2022-07-09 02:39:26 +0900

    implement: ダイアログに表示する「但し書き」を実装。

--- src/components/analysis/ucscreeninfosatisfaction/UcRelatedDiagramPropertiesEditDialog.vue
+++ src/components/analysis/ucscreeninfosatisfaction/UcRelatedDiagramPropertiesEditDialog.vue
@@ -1,7 +1,26 @@
 <template>
   <DiagramPropertiesEditDialog ref="dialog">
     <template #headMessage>
-        ※建設予定地
+      <v-alert border="left" elevation="2" colored-border>
+        {{ usecase.type.name }}
+        <v-chip color="primary" dark draggable>
+          <v-icon>{{ usecase.type.iconKey }}</v-icon>
+          {{ usecase.name }}
+        </v-chip>
+
+        を初期配置した、{{ diagram.type.name }}
+
+        <v-chip
+          outlined
+          label
+          color="info"
+        >
+          <v-icon>{{ diagram.type.iconKey }}</v-icon>
+          {{ diagram.name }}
+        </v-chip>
+
+        を作成します。
+      </v-alert>
     </template>
   </DiagramPropertiesEditDialog>
 </template>
@@ -16,9 +35,15 @@ import Resource from '@/domain/resource/Resource'
   components: { DiagramPropertiesEditDialog },
 })
 export default class UcRelatedDiagramPropertiesEditDialog extends Vue {
+  usecase!: Resource
+  diagram!: Diagram
+
   // public methods.
 
-  showOf(diagram: Diagram, _usecase: Resource): Promise<Diagram> {
+  showOf(diagram: Diagram, usecase: Resource): Promise<Diagram> {
+    this.usecase = usecase
+    this.diagram = diagram
+
     const dialog = this.$refs.dialog as DiagramPropertiesEditDialog
     return dialog.showOf(diagram)
   }
