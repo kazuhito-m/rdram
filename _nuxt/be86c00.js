c05c4e06909f52f308a37a1a9c7a6164670ad031
kazuhito-m <kazuhito-m@users.noreply.github.com>
2022-07-09 05:26:07 +0000

    deploy: 1d5deceef74aecf72815f1a371bb2f026434707f

--- _nuxt/be86c00.js
+++ _nuxt/be86c00.js
@@ -710,7 +710,7 @@ class NodeBase {
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function(module) {var require;//! moment.js
-//! version : 2.29.3
+//! version : 2.29.4
 //! authors : Tim Wood, Iskren Chernev, Moment.js contributors
 //! license : MIT
 //! momentjs.com
@@ -3164,7 +3164,7 @@ class NodeBase {
     function preprocessRFC2822(s) {
         // Remove comments and folding whitespace and replace multiple-spaces with a single space
         return s
-            .replace(/\([^)]*\)|[\n\t]/g, ' ')
+            .replace(/\([^()]*\)|[\n\t]/g, ' ')
             .replace(/(\s\s+)/g, ' ')
             .replace(/^\s\s*/, '')
             .replace(/\s\s*$/, '');
@@ -6345,7 +6345,7 @@ class NodeBase {
 
     //! moment.js
 
-    hooks.version = '2.29.3';
+    hooks.version = '2.29.4';
 
     setHookCallback(createLocal);
 
